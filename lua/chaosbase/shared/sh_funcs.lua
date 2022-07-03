177d041ecee58bb304331154b4c64dc64c17fd55
ChaosNarwhal <esdpro117@gmail.com>
2022-07-03 17:41:44 -0400

    Moved Ammo functions to its own file.

--- lua/chaosbase/shared/sh_funcs.lua
+++ lua/chaosbase/shared/sh_funcs.lua
@@ -117,44 +117,6 @@ function ChaosBase.SetBodyGroups(mdl, bodygroups)
     end
 end
 
-ChaosBaseAmmoTypes = {}
-function ChaosBase:AddAmmoType(tbl)
-	table.Add(ChaosBaseAmmoTypes, tbl)
-end
-
-local batteryammo = {{
-	Name = "ammo_chaos_battery",
-	Text = "Don't give yourself this ammo. It will only break your weapons.",
-	DMG = DMG_BULLET,
-	DamagePlayer = 0,
-	DamageNPC = 0,
-	Tracer = TRACER_LINE_AND_WHIZ,
-	Force = 500,
-	SplashMin = 5,
-	SplashMax = 10,
-	MaxCarry = 100,
-}}
-ChaosBase:AddAmmoType(batteryammo)
-
-hook.Add("Initialize", "chaos_SetupAmmoTypes", function()
-	for k,v in pairs(ChaosBaseAmmoTypes) do
-		if CLIENT then
-			language.Add(""..v.Name.."_ammo", v.Text)
-		end
-
-		game.AddAmmoType({
-		name = v.Name,
-		dmgtype = v.DMG,
-		tracer = v.Tracer,
-		plydmg = v.DamagePlayer,
-		npcdmg = v.DamageNPC,
-		force = v.Force,
-		minsplash = v.SplashMin,
-		maxsplash = v.SplashMax,
-		maxcarry = v.MaxCarry
-		})
-	end
-end)
 
 function ChaosBase:DLight(ent, pos, col, size, lifetime, emissive)
 	local HDR = render.GetHDREnabled()
